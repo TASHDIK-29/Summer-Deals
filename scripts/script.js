9686e7203133f5d007ccbebe24230dd3f6c85981
Tashdik <s2110477153@ru.ac.bd>
2024-02-17 03:34:45 +0600

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function selectCard(id){
+    
+
+    // console.log(element.querySelector('h2').innerText);
+    // console.log(element.querySelector('p').innerText);
+    addProductById(id);
+    calculateTotalById(id);
+}
---
+++ scripts/utility.js
@@ -0,0 +1,36 @@
+let count = 1 ;
+let cost = 0;
+let finalCost = 0;
+function addProductById(elementId){
+    const element = document.getElementById(elementId);
+    const elementText = element.querySelector('h2').innerText;
+    
+    createNewElement(elementText)
+}
+
+function createNewElement(text){
+    const expectedElement = document.getElementById('item-list');
+    
+    const p = document.createElement('p');
+    p.innerText =count+'. '+ text ;
+    count++;
+    expectedElement.append(p);
+    
+}
+
+function calculateTotalById(elementId){
+    const element = document.getElementById(elementId);
+    const elementText = element.querySelector('p').innerText;
+    
+    const elementArray = elementText.split(' ');
+    const price =elementArray[1];
+    const expectedPrice = parseFloat(price);
+    
+    
+    cost += expectedPrice;
+    // update Total
+    document.getElementById('total-price').innerText = cost ;
+    document.getElementById('final-price').innerText = cost ;
+    
+
+}
